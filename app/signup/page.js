10cbec5fa6fce8e2b4ccaab78fd6eea198edde8d
SubhamDeb <subham.deb.1253@gmail.com>
2025-03-09 01:23:41 +0530

    remove ids

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,31 +1,13 @@
 "use client"
-import { signIn, signOut, useSession } from "next-auth/react";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React from "react"
 
 export default function Signup() {
-  const { data: session } = useSession();
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    if (session) {
-      axios.post("http://localhost:5000/auth/google/callback", { token: session.accessToken })
-        .then(res => setUser(res.data))
-        .catch(err => console.error(err));
-    }
-  }, [session]);
-
-  return (
-    <div>
-      <h1>Next.js Authentication</h1>
-      {!session ? (
-        <button onClick={() => signIn("google")}>Login with Google</button>
-      ) : (
-        <>
-          <p>Welcome, {session.user.name}</p>
-          <button onClick={() => signOut()}>Logout</button>
-        </>
-      )}
-    </div>
-  );
+  let clientId = process.env.clientId
+  // clientId = 'fsdf'
+    return (
+      <>
+        <h3>Subham</h3>
+        <p>Client Id:: {clientId}</p>
+      </>
+    )
 }
--- controller/googleProviderController/nextAuth.js
+++ controller/googleProviderController/nextAuth.js
@@ -5,8 +5,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOption = {
     providers : [
         GoogleProvider({
-            clientId: '623781784297-cl2jt1gresjnij55bblrfpsn4rkkuech.apps.googleusercontent.com',
-            clientSecret: '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'
+            clientId: process.env.clientId,
+            clientSecret: process.env.clientSecret
             })
     ]
 }
